095c55db49fdb38aa5757a1c7a22fe6e014fa14e
dibdas <dibyandu08das@gmail.com>
2024-10-23 23:11:05 +0530

    modified

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
+	// c is the context
 	console.log(c);
 	const body =  c.req.json()
 	console.log(body);
@@ -10,6 +11,25 @@ app.get('/', (c) => {
 	console.log(c.req.query("param"));
   return c.text('Hello Hono!')
 })
+
+app.use(async (c, next) => {
+	if (c.req.header("Authorization")) {
+	  // Do validation
+	  await next()
+	} else {
+	  return c.text("You dont have acces");
+	}
+  })
+
+async function middleware(c,next){
+	if(c.req.header("Authorization")){
+		next()
+	}
+	else{
+		return c.text("you dont exist")
+	}
+}
+app.use(middleware)
 
 app.post('/', (c) => {
 	console.log(c);
